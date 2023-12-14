df1bd6d6575d2cafdd23d36ca35af1f24a43863d
Alan Chung <alanchung2k2@gmail.com>
2023-12-14 18:22:48 +0900

    tab setup

--- App.tsx
+++ App.tsx
@@ -1,20 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native'
+import Matches from './tabs/Matches'
+import Settings from './tabs/Settings'
+import Standings from './tabs/Standings'
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { FontAwesome, Feather } from '@expo/vector-icons'
+
+const Tab = createBottomTabNavigator()
+const screenOptions = {
+  tabBarShowLabel: false,
+  headerShown: false
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={screenOptions}>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+        <Tab.Screen
+          name="Standings"
+          component={Standings}
+          options={{
+            tabBarIcon: ({ focused }) => {
+              return (
+                <View style={{ alignItems: "center", justifyContent: "center" }}>
+                  <FontAwesome name="trophy" size={24} color={focused ? "black" : "silver"} />
+                  {focused ?
+                    <Text style={{ fontSize: 12, color: "black" }}>Standings</Text> :
+                    <Text style={{ fontSize: 12, color: "silver" }}>Standings</Text>
+                  }
+                </View>
+              )
+            }
+          }}
+        />
+
+        <Tab.Screen
+          name="Matches"
+          component={Matches}
+          options={{
+            tabBarIcon: ({ focused }) => {
+              return (
+                <View style={{ alignItems: "center", justifyContent: "center" }}>
+                  <Feather name="list" size={24} color={focused ? "black" : "silver"} />
+                  {focused ?
+                    <Text style={{ fontSize: 12, color: "black" }}>Matches</Text> :
+                    <Text style={{ fontSize: 12, color: "silver" }}>Matches</Text>
+                  }
+                </View>
+              )
+            }
+          }}
+        />
+
+        <Tab.Screen
+          name="Settings"
+          component={Settings}
+          options={{
+            tabBarIcon: ({ focused }) => {
+              return (
+                <View style={{ alignItems: "center", justifyContent: "center" }}>
+                  <FontAwesome name="gear" size={24} color={focused ? "black" : "silver"} />
+                  {focused ?
+                    <Text style={{ fontSize: 12, color: "black" }}>Settings</Text> :
+                    <Text style={{ fontSize: 12, color: "silver" }}>Settings</Text>
+                  }
+                </View>
+              )
+            }
+          }}
+        />
+
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
+}
---
+++ tabs/Standings.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native'
+
+export default function Standings() {
+  return (
+    <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
+      <Text>Standings</Text>
+    </View>
+  )
+}
